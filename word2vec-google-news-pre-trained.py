4558b5b80323f2d2166a11e0198aaa97acb129cc
YueAgar_c <kelvin20040807@gmail.com>
2024-11-19 05:00:46 +0800

    added most similar to good

--- word2vec-google-news-pre-trained.py
+++ word2vec-google-news-pre-trained.py
@@ -25,6 +25,12 @@ print("Most similar words to the word \"computer\": ")
 for word, similarity in similar_computer:
     print(word, similarity)
 
+# Get the most similar words to the word good and print them
+similar_good = model.most_similar("good")
+print("Most similar words to the word \"good\": ")
+for word, similarity in similar_good:
+    print(word, similarity)
+
 # Get the similarity between the words computer and phone and print it
 similarity_computer_phone = model.similarity("computer", "phone")
 print("Similarity between the words \"computer\" and \"phone\": ")
